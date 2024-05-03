1c01182885bcf607d960cafd8d2cae2da2e3aa13
Pablo Marino <pmarino2013@gmail.com>
2024-05-03 14:59:44 -0300

    Agrego librería para preview de markdown

--- src/components/PreviewApp.vue
+++ src/components/PreviewApp.vue
@@ -1,9 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+// import { VMarkdownView } from "vue3-markdown";
+// import Markdown from "vue3-markdown-it";
+import { MdPreview } from "md-editor-v3";
+import "md-editor-v3/lib/preview.css";
+
+const content = ref("# Hola");
+</script>
 <template>
   <div class="mt-3">
     <h3 class="text-center text-3xl font-bold">Preview</h3>
   </div>
 
-  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"></div>
+  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <textarea v-model="content" cols="30"> </textarea>
+    <MdPreview :modelValue="content" />
+  </div>
 </template>
 <style scope></style>
