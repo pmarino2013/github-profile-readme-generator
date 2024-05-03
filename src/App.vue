daadfeffaa925803cc508404c8e32fcbafe99d1b
Pablo Marino <pmarino2013@gmail.com>
2024-05-03 12:35:10 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CabeceraApp from "./components/CabeceraApp.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="grid grid-cols-2">
+      <div>
+        <!-- carga de datos  -->
+        <CabeceraApp />
+      </div>
+      <div>
+        <!-- preview -->
+        <!-- código  -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/CabeceraApp.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="max-w-md mx-auto">
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="titulo">
+          Título
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="titulo"
+          type="text"
+          placeholder="Ingrese el título"
+        />
+      </div>
+      <div class="mb-4">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="subtitulo"
+        >
+          Subtítulo
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="subtitulo"
+          type="text"
+          placeholder="Ingrese el subtítulo"
+        />
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="acerca">
+          Acerca de mí
+        </label>
+        <textarea
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="acerca"
+          placeholder="Ingrese información sobre usted"
+        ></textarea>
+      </div>
+      <div class="mb-4">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="habilidades"
+        >
+          Habilidades
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="habilidades"
+          type="text"
+          placeholder="Ingrese sus habilidades"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="url">
+          URL
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="url"
+          type="text"
+          placeholder="Ingrese su URL"
+        />
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Enviar
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scope></style>
